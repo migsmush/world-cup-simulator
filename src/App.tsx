6b53702d12459c0b1fbf5205442cb041ce3bb742
migsmush <1.migueloyler@gmail.com>
2025-05-14 23:41:18 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold underline text-white bg-blue-400">
+        Hello World!
+      </h1>
+    </>
+  )
+}
+
+export default App
